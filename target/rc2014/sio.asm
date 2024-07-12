8ab04015370fe6013cdac348922a05492b837bf6
The Magus Gaius Mycelius, Jocundus <gaius@magesguild.io>
2024-07-12 15:02:10 -0500

    Set up interrupts.

--- target/rc2014/sio.asm
+++ target/rc2014/sio.asm
@@ -14,12 +14,6 @@
 
     SECTION KERNEL_DRV_TEXT
 
-    PUBLIC stdout_op_start
-stdout_op_start:
-    ret
-stdout_op_end:
-    ret
-
 sio_init:
     ; Initialize the ring buffer now
     xor a
@@ -61,76 +55,12 @@ sio_init:
     ret
 
 sio_read:
-    push af ; save AF
-    ;Check if buffer is full
-    ld a, (RBUFHD) ;Get the buffer head pointer
-    ld b, a ;Save it in B
-    ld a, (RBUFTL) ;Get the butter tail pointer
-    dec a ;Decrement by one
-    cp b ;Is HEAD == (TAIL - 1)?
-    jr nz, _read_okay
-;Buffer is full, flush it
-    in a, (SIO_PORTA_DATA) ;Read the overflow byte, clearing the interrupt
-    jr _read_exit
-
-_read_okay:
-    in a, (SIO_PORTA_DATA)
-    ld hl, RBUF
-    ld l, b
-    ld (hl), a
-    ld a, l
-    inc a
-    and RING_BUF_SIZE
-    ld (RBUFHD), a
-
-_read_exit:
-    pop af
-    ei
-    reti
-
-    PUBLIC stdout_print_buffer
-stdout_print_buffer:
 sio_write:
-    push af
-    call _write_byte
-    jr nc, _write_exit
-    ld a, 0x28
-    out (SIO_PORTA_CTRL), a
-
-_write_byte:
-    di
-    ld a, (RBUFTL)
-    ld b, a
-    ld a, (RBUFHD)
-    cp b
-    jr nz, _write_okay
-
-    scf
-    jr _write_exit
-
-_write_okay:
-    ld hl, RBUF
-    ld l, b
-    ld a, (hl)
-    out (SIO_PORTA_DATA), a
-    ld a, l
-    inc a
-    and RING_BUF_SIZE
-    ld (RBUFTL), a
-    or a
-
-_write_exit:
-    pop af
-    ei
-    reti
-
 sio_open:
 sio_close:
 sio_seek:
 sio_ioctl:
 sio_deinit:
-    xor a ; Success
-    ret
 
     ; Size of the ringbuffer
     DEFC RING_BUF_SIZE = 0xF
