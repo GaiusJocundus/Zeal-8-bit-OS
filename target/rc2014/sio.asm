7fc86221d8af2cac90b06598450e3773ab9e0195
The Magus Gaius Mycelius, Jocundus <gaius@magesguild.io>
2024-07-12 17:51:18 -0500

    Add stdout setup.

--- target/rc2014/sio.asm
+++ target/rc2014/sio.asm
@@ -6,6 +6,7 @@
     INCLUDE "drivers_h.asm"
     INCLUDE "utils_h.asm"
     INCLUDE "stdout_h.asm"
+    INCLUDE "interrupt_vect.asm"
 
     DEFC SIO_PORTA_CTRL = 0x80
     DEFC SIO_PORTA_DATA = 0x81
@@ -21,6 +22,8 @@ sio_init:
     ld (hl), a
     ld hl, RBUFTL
     ld (hl), a
+
+    EXTERN zos_vfs_set_stdout
 
     ; Configure register 1 to have a single interrupt vector for all status, enable Rx Interrupts
     ld a, 1  ; reg to configure
@@ -52,23 +55,78 @@ sio_init:
     ld a, 0x68
     out (SIO_PORTA_CTRL), a
 
-    ret
+    ld hl, this_struct
+    call zos_vfs_set_stdout
 
+    INTERRUPTS_ENABLE()
+
+    PUBLIC interrupt_default_handler
+    PUBLIC interrupt_sio_handler
+interrupt_default_handler:
+interrupt_sio_handler:
+    ENTER_CRITICAL()
+    push af
+;Check if buffer is full
+    ld a,(RBUFHD)
+    ld b,a
+    ld a,(RBUFTL)
+    dec a
+    and RBUF_SIZE
+    cp b
+    jr nz,sio_read
+    in a,(SIO_PORTA_DATA)
+    jr _sio_read_exit
 sio_read:
+    in a,(SIO_PORTA_DATA)
+    ld hl,RBUF
+    ld l,b
+    ld (hl),a
+_sio_read_exit:
+    pop af
+    EXIT_CRITICAL()
+    reti
+
 sio_write:
+    ENTER_CRITICAL()
+    push af
+    ld a,(RBUFTL)
+    ld b,a
+    ld a,(RBUFHD)
+    cp b
+    jr nz,_sio_write_okay
+;Buffer is empty, set carry flag and exit
+    scf
+    jp _sio_write_exit
+_sio_write_okay:
+    ld hl,RBUF
+    ld l,b
+    ld a,(hl)
+    out (SIO_PORTA_DATA),a
+    ld a,l
+    inc a
+    and RBUF_SIZE
+    ld (RBUFTL),a
+    or a
+_sio_write_exit:
+    pop af
+    EXIT_CRITICAL()
+    reti
+
 sio_open:
 sio_close:
 sio_seek:
 sio_ioctl:
 sio_deinit:
+    xor a
+    ret
 
     ; Size of the ringbuffer
-    DEFC RING_BUF_SIZE = 0xF
+    DEFC RBUF_SIZE = 0xF
 
     SECTION DRIVER_BSS
 RBUFHD: DEFS 1 ; allocate 1 byte
 RBUFTL: DEFS 1 ; allocate 1 byte
-RBUF: DEFS RING_BUF_SIZE ; Allocate RING_BUF_SIZE bytes for the buffer itself
+RBUF: DEFS RBUF_SIZE ; Allocate RBUF_SIZE bytes for the buffer itself
 
     SECTION KERNEL_DRV_VECTOR
 this_struct:
